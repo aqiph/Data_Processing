bfe5a4e078507233f5a5833db1ac7f64797cde13
aqiph <aqiph818@gmail.com>
2025-07-28 00:09:38 +0800

    Update utils/prepare_boltz_yaml.py to process pocket constraints.

--- utils/prepare_boltz_yaml.py
+++ utils/prepare_boltz_yaml.py
@@ -8,12 +8,12 @@ Created on Mon Jul 14 09:08:00 2025
 
 import os
 import pandas as pd
-import yaml
+from ruamel.yaml import YAML
 import string
 from tqdm import tqdm
 
 
-def generate_boltz_yaml(output_dir, polymers, ligand_id, ligand_smiles, affinity=True):
+def generate_boltz_yaml(output_dir, polymers, ligand_id, ligand_smiles, affinity=True, constraints=None):
     """
     Generate a single yaml file for Boltz-2.
     :param output_dir: str, path of the output directory.
@@ -24,6 +24,9 @@ def generate_boltz_yaml(output_dir, polymers, ligand_id, ligand_smiles, affinity
     :param ligand_id: str, id of the ligand.
     :param ligand_smiles: str, SMILES of the ligand.
     :param affinity: bool, whether to include affinity in the yaml file.
+    :param constraints: list, list of constraints, using the format:
+    [{"pocket": {"binder": "X", "contacts": "[ [ A1, 829 ], [ A1, 138 ] ]"}}
+    ...]
     """
     os.makedirs(output_dir, exist_ok=True)
 
@@ -47,10 +50,19 @@ def generate_boltz_yaml(output_dir, polymers, ligand_id, ligand_smiles, affinity
     }
     if affinity:
         data["properties"] = [{"affinity": {"binder": ligand_chain_id}}]
+    # define pocket
+    if constraints is not None: # [{"pocket": {"binder": "X", "contacts": "[ [ A1, 829 ], [ A1, 138 ] ]"}}]
+        for const in constraints:
+            for const_type, info in const.items():
+                if const_type == 'pocket':
+                    info['binder'] = ligand_chain_id
+                    # info['contacts'] = str(info['contacts'])
+        data["constraints"] = constraints
 
     output_file = os.path.join(output_dir, f"{ligand_id}.yaml")
     with open(output_file, "w") as f:
-        yaml.dump(data, f, sort_keys=False)
+        yaml = YAML()
+        yaml.dump(data, f)
 
 
 def batch_generate_boltz_yaml(output_dir, input_file_polymers, input_file_SMILES, id_column_name='ID', smiles_column_name='Cleaned_SMILES', affinity=True):
@@ -66,8 +78,9 @@ def batch_generate_boltz_yaml(output_dir, input_file_polymers, input_file_SMILES
     os.makedirs(output_dir, exist_ok=True)
 
     # process polymers
+    yaml = YAML()
     with open(input_file_polymers, "r") as f:
-        doc = yaml.safe_load(f)
+        doc = yaml.load(f)
     polymers = []
     for entry in doc.get("sequences", []):
         # entry is a dict with a single key in ["protein", "dna", or "rna"], {<ENTITY_TYPE>: {"id": <CHAIN_ID>, "sequence": <sequence>}}
@@ -75,7 +88,7 @@ def batch_generate_boltz_yaml(output_dir, input_file_polymers, input_file_SMILES
             entry_type = entry_type.lower()
             if entry_type in {"protein", "dna", "rna"}:
                 chain_id = info.get("id")
-                sequence = info.get("sequence")
+                sequence = info.get("sequence").strip()
                 if not chain_id or not sequence:
                     raise ValueError(f"Missing chain_id or sequence in {entry_type} entry")
                 polymers.append({entry_type: {"id": chain_id, "sequence": sequence}})
@@ -89,7 +102,7 @@ def batch_generate_boltz_yaml(output_dir, input_file_polymers, input_file_SMILES
     # batch generation
     for idx in tqdm(range(len(ligand_IDs))):
         ligand_id, ligand_smiles = ligand_IDs[idx], ligand_SMILES[idx]
-        generate_boltz_yaml(output_dir, polymers, ligand_id, ligand_smiles, affinity)
+        generate_boltz_yaml(output_dir, polymers, ligand_id, ligand_smiles, affinity, constraints=doc.get("constraints", None))
 
 
 if __name__ == "__main__":
